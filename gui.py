bd77c719cf32a10801e52a938f16c60a1d0d4b58
Brett <brettgurman@gmail.com>
2014-11-03 17:52:45 -0500

    improved gui.py

--- gui.py
+++ gui.py
@@ -2,60 +2,84 @@ from Tkinter import *
 import ttk
 import multiprocessing
 import sftpbackup_util as util
+root = Tk()
+root.title("SFTP Backup")
 
+dictionary = {}
+
+servername_var=StringVar()
+destination_var=StringVar()
+username_var=StringVar()
+password_var=StringVar()
+pattern_var=StringVar()
+function_var=StringVar()
+frequency_var=IntVar()
+frequency_units_var=StringVar()
+maxsize_var=StringVar()
+maxsize_units_var=StringVar()
+folder_var=StringVar()
+store_var = BooleanVar()
+bool_var = BooleanVar()
+bool_var=False
+
+def submit():
+        response = pack_response()
+        bool_var = (True)
+        return response
 
 def browse():
         print "browsing"
 
-def submit(child_connection):
-        response = pack_response()
-        child_connection.send(response)
-
 def pack_response():
-        response = {'pattern'   : pattern_var.get(),
-                    'time'      : frequency_var.get(),
-                    'function'  : function_var.get(),
+        response = {#'pattern'   : pattern_var.get(),
+                    'pattern'    : "one-time",
+                    'time'      : to_seconds(int(frequency_var.get())),
+                    #'function'  : function_var.get(),
+                    'function'  : util.backup_folder_simple,
                     'store'     : store_var.get(),
-                    'prefs'     : pack_prefs()
-                   }
+                    'prefs'     : pack_prefs() }
         return response
 
 
 def pack_prefs():
-        prerencesfs = {'maxsize': maxsize_var.get()
-                       'server' : servername_var.get()
-                       'user'   : username_var.get()
-                       'pass'   : password_var.get()
-                   'destination': destination_var.get()
-                       'folder' : folder_var.get()
-                      }
+        preferences = {'maxsize': to_bytes(int(maxsize_var.get())),
+                       'server' : servername_var.get(),
+                       'user'   : username_var.get(),
+                       'pass'   : password_var.get(),
+                   'destination': destination_var.get(),
+                       'folder' : folder_var.get() }
 
-        return prefs
+        return preferences
 
-def display_error(error):
-        function()
+def to_seconds(time):
+        convert = {'seconds'    : 1,
+                   'minutes'    : 60,
+                   'hours'      : 60*60,
+                   'days'       : 60*60*24,
+                   'weeks'      : 60*60*24*7,
+                   'months'     : 60*60*24*7*4,
+                   'years'      : 60*60*24*7*4*12 }
+
+        return time/convert[frequency_units_var.get()]
+
+def to_bytes(size):
+        convert = {'bytes'      : 1,
+                   'kilobytes'  : 1000,
+                   'megabytes'  : 1000000,
+                   'gigabytes'  : 1000000000,
+                   'terabytes'  : 1000000000000 }
+
+        return int(maxsize_var.get())/convert[maxsize_units_var.get()]
 
 def start(child_connection):
-        root = Tk()
-        root.title("SFTP Backup")
+
 
         mainframe = ttk.Frame(root, padding="3 3 12 12")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         mainframe.columnconfigure(0, weight=1)
         mainframe.rowconfigure(0, weight=1)
 
-        servername_var=StringVar()
-        destination_var=StringVar()
-        username_var=StringVar()
-        password_var=StringVar()
-        pattern_var=StringVar()
-        function_var=StringVar()
-        frequency_var=StringVar()
-        frequency_units_var=StringVar()
-        maxsize_var=StringVar()
-        maxsize_units_var=StringVar()
-        folder_var=StringVar()
-        store_var = StringVar()
+
 
 
         ttk.Label(mainframe, text="-- Server information --").grid(column=1, row=1)
@@ -78,10 +102,14 @@ def start(child_connection):
 
 
         ttk.Label(mainframe, text="Pattern").grid(row=2, column=3)
-        pattern_box = ttk.Combobox(mainframe, textvariable=pattern_var).grid(row=3, column=3)
+        pattern_box = ttk.Combobox(mainframe, textvariable=pattern_var)
+        pattern_box.grid(row=3, column=3)
+        pattern_box['values']=('repeating', 'one-time')
 
         ttk.Label(mainframe, text="Function").grid(row=2, column=4)
-        function_box = ttk.Combobox(mainframe, textvariable=function_var).grid(row=3, column=4)
+        function_box = ttk.Combobox(mainframe, textvariable=function_var)
+        function_box.grid(row=3, column=4)
+        function_box['values'] = ('backup folder history', 'backup folder simple')
 
         ttk.Label(mainframe, text="Frequency").grid(row=5, column=3)
         ttk.Entry(mainframe,textvariable=frequency_var).grid(row=6, column=3)
@@ -99,16 +127,20 @@ def start(child_connection):
         ttk.Entry(mainframe, textvariable=folder_var).grid(row=3, column=5)
         ttk.Button(mainframe, text="Browse", command=browse).grid(row=4, column=5)
 
-        ttk.Button(mainframe, text="EXECUTE", command=submit).grid(row=6, column=5)
+        ttk.Label(mainframe, text="Valid?").grid(row=6, column=5, sticky=W)
+        ttk.Label(mainframe, text="IDK!").grid(row=6, column=5)
 
+        ttk.Button(mainframe, text="EXECUTE", command=submit).grid(row=7, column=5)
 
+        ttk.Radiobutton(mainframe, text="yes", variable=store_var, value=True).grid(row=5, column=5)
+        ttk.Radiobutton(mainframe, text="no", variable=store_var, value=False).grid(row=5,column=5, sticky=E)
+        ttk.Label(mainframe, text="Store?").grid(row=5, column=5, sticky=W)
 
         for child in mainframe.winfo_children():
                 child.grid_configure(padx=5, pady=5)
 
-
-
-
         root.mainloop()
+        
 
+start(0)
 
